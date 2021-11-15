3ea413f8ebdc0112b757056e17119154cbc74007
k-sandi <kurniya097@gmail.com>
2021-11-15 13:41:16 +0700

    init project | checkpoint on variable and data type

---
+++ string.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Kurnia")
+	fmt.Println("Kurnia Sandi")
+	fmt.Println("**************")
+	fmt.Println(len("Kurnia"))     //mengetahui panjang karakter
+	fmt.Println("Kurnia Sandi"[0]) //mengambil baris ke-berapa (tetapi yang ditampilkan adalah byte-nya)
+	fmt.Println("Kurnia Sandi"[1])
+}
---
+++ variable.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	var name string  //Variable Declaration
+
+	name = "Kurnia Sandi"
+	fmt.Println(name)
+	
+	name = "Sandi Booriel"
+	fmt.Println(name)
+	
+}
