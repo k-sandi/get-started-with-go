d6e1db3a2d0fe807071fbe2b7691a4566dfecf94
k-sandi <kurniya097@gmail.com>
2022-12-01 19:09:23 +0800

    gopath configuration

---
+++ 42_gopath.go
@@ -0,0 +1,10 @@
+/*
+pre:
+Di versi Go-Lang terbaru, sekarang sudah tidak direkomendasikan lagi menggunakan GOPATH, melainkan lebih direkomendasikan menggunakan Go Modules.
+Karena Go Modules akan dibahas di kelas selanjutnya, dan kita akan menggunakan GOPATH, jadi silahkan gunakan perintah berikut di terminal untuk mengaktifkan GOPATH "go env -w GO111MODULE=off"
+=======
+
+GOPATH
+- GOPATH adalah env variable yang berisikan lokasi tempat kita menyimpan project dan library Go-Lang.
+- GOPATH wajib dibuat ketika kita mulai membuat aplikasi lebih dari satu file atau butuh menggunakan library dari luar (import resource). 
+*/
